c166b14da5f0213dae424b4688469e88007b7821
duySWEtdtu0406 <48251926+duySWEtdtu0406@users.noreply.github.com>
2020-10-16 18:05:49 +0700

    Duy changed

--- index.php
+++ index.php
@@ -10,28 +10,17 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="view/style.css"/>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"/>
     <link rel="icon" href="https://upload.wikimedia.org/wikipedia/vi/1/1b/T%C4%90T_logo.png" type="image/x-icon">
-    <link rel="stylesheet" href="style.css"/>
+    
     <script src="javascript/main.js"></script>
+    
     <title>Login</title>
 </head>
-<style>
-
-.card{
-  margin:auto;
-  margin-top: 15rem;
-  width: 400px;
-  height: 400px;
-  display: flex;
-  flex: wrap;
-  box-shadow: 5px 10px  rgb(109, 179, 219); 
-  border: 2px solid rgb(109, 179, 219);
-}
-</style>
 <body>
     <!-- Material form login -->
-    <div class="card p-3">
+    <div class="Login-card p-3">
         <!--Card content-->
         <div class="card-body px-lg-5 pt-0 mt-5">
 
@@ -39,15 +28,15 @@
             <form class="text-center"  action="Handle/LoginProcess.php" method="POST">
                 <!-- Email -->
                     <div class="md-form">
-                        <span>Tài Khoản</span>  <input type="text"  name="txtUserName" id="txtUserName" placeholder="User Name" required class="form-control">
+                        <input type="text"  name="txtUserName" id="txtUserName" placeholder="User Name" required class="form-control">
                     </div>
                     <!-- Password -->
-                    <div class="md-form">
-                        <span>Mật Khẩu<input type="password" name="txtPassword" id="txtPassword" placeholder="Password" required class="form-control"></span>
+                    <div class="md-form mt-3">
+                       <input type="password" name="txtPassword" id="txtPassword" placeholder="Password" required class="form-control">
                         
                     </div>
 
-                    <div class="d-flex justify-content-around">
+                    <div class="d-flex justify-content-around mt-3" >
                         <div>
                             <!-- Remember me -->
                             <div class="form-check">
@@ -55,7 +44,7 @@
                                 <label class="form-check-label" for="materialLoginFormRemember">Remember me</label>
                             </div>
                         </div>
-                        <div>
+                        <div mt-2>
                             <!-- Forgot password -->
                             <a href="View/ForgotPassword.php">Forgot password?</a>
                         </div>
@@ -65,21 +54,13 @@
                     <button class="btn btn-outline-info btn-rounded btn-block my-4 waves-effect z-depth-0" type="submit" id="buttonLogin" name="buttonLogin" value="Login">Sign in</button>
 
                     <!-- Register -->
-                    <p>Not a member?
-                        
-                        <a href="View/SignUp.php">Register
-                        
-                    </a>
-                        
+                    <p>Don't have an account? |           
+                        <a href="View/SignUp.php">Register</a>      
                     </p>
             </form>
                 <!-- Form -->
-                
-
         </div>
-
-    </div>
-    
+    </div>  
                 <!-- Material form login -->
 </body>
 
