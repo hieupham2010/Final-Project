4c4467c6e63d48b277115ca9df87029950a96bdf
hieupham2010 <01684673046h@gmail.com>
2020-11-03 23:01:51 +0700

    edit server

--- Handle/DataAccess.php
+++ Handle/DataAccess.php
@@ -1,18 +1,18 @@
 <?php
-    // $serverName = "localhost";
-    // $Name = "root";
-    // $password = "";
-    // $dbName = "classroom";
-    // $connection = new mysqli($serverName , $Name , $password ,$dbName);
-    // if($connection->connect_error) {
-    //     die("Connection failed: " . $connection->connect_error);
-    // }
-    $serverName = "enqhzd10cxh7hv2e.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
-    $Name = "f3oyc1ddhrr2hess";
-    $password = "yy905zqedgkba6z8";
-    $dbName = "k8m29jk63f2uk9wy";
+    $serverName = "localhost";
+    $Name = "root";
+    $password = "";
+    $dbName = "classroom";
     $connection = new mysqli($serverName , $Name , $password ,$dbName);
     if($connection->connect_error) {
         die("Connection failed: " . $connection->connect_error);
     }
+    // $serverName = "enqhzd10cxh7hv2e.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
+    // $Name = "f3oyc1ddhrr2hess";
+    // $password = "yy905zqedgkba6z8";
+    // $dbName = "k8m29jk63f2uk9wy";
+    // $connection = new mysqli($serverName , $Name , $password ,$dbName);
+    // if($connection->connect_error) {
+    //     die("Connection failed: " . $connection->connect_error);
+    // }
 ?>
