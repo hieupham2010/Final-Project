a0e16d5d723217ae7a1462ad432be5b0f9190c46
hieupham2010 <01684673046h@gmail.com>
2020-11-05 16:29:01 +0700

    add

--- Handle/CreateCommentProcess.php
+++ Handle/CreateCommentProcess.php
@@ -2,7 +2,7 @@
     session_start();
     if(isset($_POST["ClassID"]) && !empty($_POST["ClassID"])
         &&isset($_POST["PostID"]) && !empty($_POST["PostID"])) {
-        $EncryptCode = $_POST["ClassID"];
+        $EncryptCode = urlencode($_POST["ClassID"]);
         if(isset($_POST["txtComment"]) && !empty($_POST["txtComment"])) {
             require_once 'DataAccess.php';
             $UserName = $_SESSION["username"];
